f3ad011a3d6fbaa620a3994156f25bffe6913b6b
kehuanhuan <1152018701@qq.com>
2017-04-19 17:24:30 +0800

    完善make_curl.sh

--- make_curl.sh
+++ make_curl.sh
@@ -7,17 +7,33 @@
 #-----------------------
 
 ###说明####
-#参数1 '请求URL'
-#参数2 '请求方式'
-#参数3 '请求参数：格式：name=abc&age=18&sex=男'
+#参数1 '请求URL, 如：www.fgcy.top'
+#参数2 '请求参数：格式：name=abc&age=18&sex=男'
+#参数3 '请求方式, '
 #参数4  header设置 ：
 
-if [ $1 ]
+
+url=''
+method='POST'
+
+if [ -z $1 ]
 then
-  echo "您输入的url为：$1"
-else
-  echo "请输入URL"
+  echo "--------------说明------------"
+  echo "#参数1 '请求URL，如：www.fgcy.top'"
+  echo "#参数2 '请求方式, 如：POST' "
+  echo "#参数3 '请求参数：格式：name=abc&age=18&sex=男'"
+  echo "#参数4  header设置 ："
+  echo "-------------------- "
+  exit
+elif [ $1 ]
+then
+  url=$1
 fi
 
+if [ $3 ]
+then
+ method=$3
+fi
 
-curl -i -H "Accept: application/json" --data "$3" -X $2 "http://$1" > curl_error.html
+curl -i -H "Accept: application/json" --data "$2" -X $method "http://$url"
+echo '\n'
