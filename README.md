ace803c8f544ce51f6cc8ba79dc4db8063efa392
khh <khh@khhdeiMac.local>
2017-09-04 19:22:29 +0800

    微调001

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 linux 中常用的的工具脚本，学习笔记等
 time_to_date 
+
+test git
--- spider/index.php
+++ spider/index.php
@@ -1,28 +1,5 @@
 <?php
 
 require 'vendor/autoload.php';
-require './Common/functions.php';
 
-
-$url='http://vote.zyctd.com/m31';//这里请填写投票的请求地址
-// $url='http://127.0.0.1:8084/test';//这里请填写投票的请求地址
-$data = ['AppID' => 2, 'voteItemId' => 31, 'Data.VoteItemId' => 31, 'Data.VoteType' => 1, 'CLickVote' => ''];
-
-$client = new GuzzleHttp\Client();
-
-$response = $client->request('POST',  $url, [
-    'form_params' => $data,
-    'headers' => [
-        'Upgrade-Insecure-Requests' => 1,
-        'User-Agent' => 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.101 Safari/537.36',
-        'Content-Type' => 'application/x-www-form-urlencoded'
-    ]
-]);
-
-// Get all of the response headers.
-foreach ($response->getHeaders() as $name => $values) {
-    echo $name . ': ' . implode(', ', $values) . "\n";
-}
-$body = $response->getBody();
-$remainingBytes = $body->getContents();
-var_export($remainingBytes);
+echo randChinaIp();
