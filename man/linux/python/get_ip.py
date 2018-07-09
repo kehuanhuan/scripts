9fb2bdaad114182a80eb433831d533ae4790ce52
kehuanhuan <huanhuan@yuanli-inc.com>
2018-07-09 20:38:34 +0800

    fix 0709

--- man/linux/python/get_ip.py
+++ man/linux/python/get_ip.py
@@ -2,19 +2,25 @@
 # -*- coding: UTF-8 -*-
 
 import sys
+import socket
 args = sys.argv
 
 def main():
     try:
         if args[1] == 'ip':
             get_ip()
+        if args[1] == 'hostname':
+            get_hostname()
     except:
         pass
 
 def get_ip():
-    import socket
     ip = socket.gethostbyname(socket.gethostname())
     print(ip)
+
+def get_hostname():
+    hostname = socket.gethostname()
+    print(hostname)
 
 if __name__ == '__main__':
     main()
