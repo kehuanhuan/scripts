07275a5dfa2065d8878d7bfea09b2f9e1f3d2c25
kehuahuan <1152018701@qq.com>
2019-01-28 15:37:38 +0800

    fix new content

--- code/python/khh.py
+++ code/python/khh.py
@@ -1,9 +1,21 @@
-#!/usr/bin/python
-# -*- coding: UTF-8 -*-
+# -*- coding: utf-8 -*-
 
-# 导入 Phone 包
-from package.khh1 import khh1
-from package.khh2 import khh2
+import scrapy
 
-khh1()
-khh2()
+class ToScrapeCSSSpider(scrapy.Spider):
+    name = "toscrape-css"
+    start_urls = [
+        'http://quotes.toscrape.com/',
+    ]
+
+    def parse(self, response):
+        for quote in response.css("div.quote"):
+            yield {
+                'text': quote.css("span.text::text").extract_first(),
+                'author': quote.css("small.author::text").extract_first(),
+                'tags': quote.css("div.tags > a.tag::text").extract()
+            }
+
+        next_page_url = response.css("li.next > a::attr(href)").extract_first()
+        if next_page_url is not None:
+            yield scrapy.Request(response.urljoin(next_page_url))
--- man/linux/home.md
+++ man/linux/home.md
@@ -9,5 +9,26 @@
     - 如果要转http代理 sudo apt-get install polipo
 
 
+### ss 服务搭建过程
+
+    - sudo apt-get update
+    - sudo apt-get install python-pip
+    - pip install shadowsocks
+    - vim ssserver_config.json
+
+     - `ssserver_config.json` 文件内容如下
+    ```
+    {
+        "server": "0.0.0.0",
+        "server_port": 443,
+        "password": "ABC4957259vpn@",
+        "timeout": 300,
+        "method": "aes-256-cfb"
+    }
+    ```
+
+    - ssserver -c ssserver_config.json -d start
+
+
 
 
